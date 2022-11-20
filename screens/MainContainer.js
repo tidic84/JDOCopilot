b05325a70dd899e1757489af7be7faf466bf8f5e
Tidic <tidic84@gmail.com>
2022-11-20 18:30:39 +0100

    debut navbar

---
+++ screens/MainContainer.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons';
+
+// Screens
+import HomeScreen from './homeScreen';
+import ChatScreen from './LoginScreen';
+//import SettingsScreen from './SettingsScreen';
+
+//Screen names
+const homeName = "Home";
+const chatName = "Chat";
+//const settingsName = "Settings";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === chatName) {
+              iconName = focused ? 'list' : 'list-outline';
+
+            } /*else if (rn === settingsName) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }*/
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={detailsName} component={DetailsScreen} />
+        <Tab.Screen name={settingsName} component={SettingsScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
