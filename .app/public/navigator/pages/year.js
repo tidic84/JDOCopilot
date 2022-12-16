90a5c3ef290f544c9e127b0cbc13bd45e94c4912
Tidic <tidic84@gmail.com>
2022-12-16 07:53:00 +0000

    debut année

--- .app/public/navigator/pages/year.js
+++ .app/public/navigator/pages/year.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Text, View, TextInput, TouchableOpacity, Image } from "react-native";
 import {defaultCSS} from "../../stylesheets/_default/year"
+import { timeDifference } from "../../util/relativeDays";
+let dataReady = false
 
 export default class Year extends React.Component {
+    getHolidays = async () => {
+        if (!dataReady) {
+            const holidays = await (await fetch("https://jdocopilot-api.herokuapp.com/holidays")).json();
+            this.setState({ holidays: holidays });
+            dataReady = true;
+        }
+
+    }
     render() {
-        return (
-            <View style={defaultCSS.container}>
-                <Text style={defaultCSS.text}>Year</Text>
-            </View>
-        );
+        this.getHolidays();
+        if(dataReady){
+            return (
+                <View style={defaultCSS.container}>
+                    <Text style={defaultCSS.text}>Vacances</Text>
+                    <Text style={defaultCSS.text}>{this.state.holidays[0].title}</Text>
+                    <Text style={defaultCSS.text}>{timeDifference(Date.now() + 3600000, new Date(this.state.holidays[0].dateStart).getTime())}</Text>
+                </View>
+            );
+        } else {
+            return (
+                <View style={defaultCSS.container}>
+                </View>
+            );
+        }
     }
 }
